90b180559cf8d0436f3b40c1ef1323418f17e48b
Nicozilabe <nicozilabe@hotmail.com>
2024-02-08 20:43:46 -0300

    funciona a medias el condicional

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //alert("Hola este es mi Javascript");
+//let nombre = "Nicolás"
+//console.log(nombre)
+
+let cont_tit = "Acerca de mí";
+
+let tit = document.querySelectorAll('.tit');
+tit.innerHTML = cont_tit;
+
+let texttit = tit.innerHTML;
+console.log(texttit);
+console.log(tit.innerHTML);
+
+if (texttit == "Acerca de mí") {
+    tit.innerHTML = "lo que se me canta";
+    console.log(tit.innerHTML);
+};
